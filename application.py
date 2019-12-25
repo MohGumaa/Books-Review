089f7f688d351ae026f2c4d9fbeeb58d5e0ea500
MohGumaa <gomaam14@gmail.com>
2019-12-25 15:57:26 +0530

    modify some line

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from werkzeug.security import check_password_hash, generate_password_hash
 from helper import login_required
 
-
 app = Flask(__name__)
 
 # Check for environment variable
@@ -16,14 +15,13 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
 Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
 
 # Index
 @app.route("/")
